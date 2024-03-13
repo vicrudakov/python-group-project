01f107ae9ba8a87fcf4b398a00be49a532e7e2be
Maria Levina <sumatrana@yandex.ru>
2024-03-13 16:47:06 +0300

    add test structure for ClassifyWineData class

---
+++ tests/test_classify_wine_data.py
@@ -0,0 +1,21 @@
+import pytest
+from wine_data_analysis.classify_wine_data import ClassifyWineData
+
+
+def test_logistic_regression():
+    pass
+
+def test_decision_tree():
+    pass
+
+def test_knn():
+    pass
+
+def test_accuracy():
+    pass
+
+def test_average_precision():
+    pass
+
+def test_average_recall():
+    pass
